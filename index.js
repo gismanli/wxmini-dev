2b3683c3ed45b00050225ea69ccd20cff214b6ce
gismanli <gismanli@tencent.com>
2024-12-16 11:33:21 +0800

    feat: 增加推送按钮

--- index.js
+++ index.js
@@ -49,6 +49,47 @@ app.get("/api/wx_openid", async (req, res) => {
   }
 });
 
+app.get("/send", async function (req, res) {
+  const { openid } = req.query; // 通过get参数形式指定openid
+  // 在这里直接是触发性发送，也可以自己跟业务做绑定，改成事件性发送
+  const info = await sendapi(openid);
+  res.send(info);
+});
+
+async function sendapi(openid) {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        url: "http://api.weixin.qq.com/cgi-bin/message/subscribe/send",
+        method: "POST",
+        body: JSON.stringify({
+          touser: openid,
+          template_id: "YGfYNyOZc0VfALegCjHBqenP9QhupnHReRQNpJlWwnM",
+          miniprogram_state: "developer",
+          data: {
+            // 这里替换成自己的模板ID的详细事项，不要擅自添加或更改
+            // 按照key前面的类型，对照参数限制填写，否则都会发送不成功
+            //
+            phrase4: {
+              value: "这是一个提醒",
+            },
+            time15: {
+              value: new Date().toLocaleDateString(),
+            },
+            character_string1: {
+              value: openid
+            }
+          },
+        }),
+      },
+      function (error, res) {
+        if (error) reject(error);
+        resolve(res.body);
+      }
+    );
+  });
+}
+
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
